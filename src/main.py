3c1b7c2ba409c99f37332c578bfce74478e4de40
Rafael Carneiro <ext.rafael.carneiro@farfetch.com>
2022-09-20 12:17:10 +0100

    Support Swiss

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from util.constants import *
 from config.strings import text_large_result
 from config.commands import command_initial
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
   
 @client.event
 async def on_ready(): 
